542ac8ac6bcef272c94eed03af711eee59136808
mayurnikov <mayurnikov@zfort.com>
2023-08-27 16:12:54 +0300

    Fixed

--- index.js
+++ index.js
@@ -26,54 +26,52 @@ class dac5771{
         this.address = address;
         this.wire = new i2c(this.address,{device: this.device});
     }
-}
 
-/**
- * Shift data to the right byte-format and send it to the dac5771
- *
- * @param  {number} value
- */
+    /**
+    * Shift data to the right byte-format and send it to the dac5771
+    *
+    * @param  {number} value
+    */
 
-dac5771.setDACRaw = function(value, callback)
-{
-    var self = this;
-    var byte0 = (value << 4) & 0xF0;                           //Most
-    var byte1 = ((value & 0xF0)>>4) | REG_CONFIG_OP_NORMAL;    //Least
-    //self.wire.writeBytes(this.address, [byte1, byte0], function(err)
-    self.wire.writeByte(byte1, function(err)
+    setDACRaw(value, callback)
     {
-        if(err)
+        var self = this;
+        var byte0 = (value << 4) & 0xF0;                           //Most
+        var byte1 = ((value & 0xF0)>>4) | REG_CONFIG_OP_NORMAL;    //Least
+        //self.wire.writeBytes(this.address, [byte1, byte0], function(err)
+        self.wire.writeByte(byte1, function(err)
         {
-            callback(err);
-        }
-    });
-    self.wire.writeByte(byte0, function(err)
+            if(err)
+            {
+                callback(err);
+            }
+        });
+        self.wire.writeByte(byte0, function(err)
+        {
+            if(err)
+            {
+                callback(err);
+            }
+        });
+    }
+
+    /**
+    * Calculate the right dac-value in relation of the passed voltage 
+    *
+    * @param  {number} value
+    */
+    setDACVoltage (voltage, callback)
     {
-        if(err)
+        var self = this;
+        var dacValue = DACVALUE_FACT * voltage;
+        self.setDACRaw(this.address, dacValue, function(err)
         {
-            callback(err);
-        }
-    });
-}
-
-/**
- * Calculate the right dac-value in relation of the passed voltage 
- *
- * @param  {number} value
- */
-
-
-dac5771.setDACVoltage = function(voltage, callback)
-{
-    var self = this;
-    var dacValue = DACVALUE_FACT * voltage;
-    self.setDACRaw(this.address, dacValue, function(err)
-    {
-        if(err)
-        {
-            callback(err);
-        }
-    });
+            if(err)
+            {
+                callback(err);
+            }
+        });
+    }
 }
 
 module.exports = dac5771;
